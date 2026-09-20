1c59e89c004a880e3eda99efb9147aff5f55e7bb
Ant1 <hamsterdevourer@proton.me>
2026-09-20 09:53:20 +0100

    removed random unused name parameter in a function

--- main_test.lua
+++ main_test.lua
@@ -4,46 +4,46 @@ local hosting
 local send_stuff
 local rpc
 function love.load()
-    local enet = require("enet")
-    rpc = require("rpc")
-    hosting = false
-    for i, arg in pairs(arg) do
-        if arg == "--host" then
-            hosting = true
-        end
-    end
-    print("hosting:",hosting)
-    if hosting then
-        host = enet.host_create("localhost:6789")
-    else
-        host = enet.host_create()
-    end
-    send_stuff = rpc.new_rpc("peer",{"string","int","double"},nil,host,function(phrase,number,double)
-        print("phrase ->",phrase,"number ->",number,"double ->",double)
-    end)
-    if not hosting then
-        server = host:connect("localhost:6789")
-        print("sending")
-        send_stuff(server:index(),"hello",5,5.32)
-    end
+	local enet = require("enet")
+	rpc = require("rpc")
+	hosting = false
+	for i, arg in pairs(arg) do
+		if arg == "--host" then
+			hosting = true
+		end
+	end
+	print("hosting:", hosting)
+	if hosting then
+		host = enet.host_create("localhost:6789")
+	else
+		host = enet.host_create()
+	end
+	send_stuff = rpc.new_rpc("peer", { "string", "int", "double" }, nil, host, function(phrase, number, double)
+		print("phrase ->", phrase, "number ->", number, "double ->", double)
+	end)
+	if not hosting then
+		server = host:connect("localhost:6789")
+		print("sending")
+		send_stuff(server:index(), "hello", 5, 5.32)
+	end
 end
 
 function love.draw()
-    local event = host:service(100)
-    while event do
-        if event.type == "receive" then
-        print("Got message: ", event.data, event.peer)
-        --event.peer:send( "pong" )
-        rpc.listen_rpc(event.data)
-        elseif event.type == "connect" then
-        print(event.peer, "connected.")
-        elseif event.type == "disconnect" then
-        print(event.peer, "disconnected.")
-        end
-        event = host:service()
-    end
-    if not hosting then
-        print("sending")
-        send_stuff(server:index(),"hello",5,5.32)
-    end
+	local event = host:service(100)
+	while event do
+		if event.type == "receive" then
+			print("Got message: ", event.data, event.peer)
+			--event.peer:send( "pong" )
+			rpc.listen_rpc(event.data)
+		elseif event.type == "connect" then
+			print(event.peer, "connected.")
+		elseif event.type == "disconnect" then
+			print(event.peer, "disconnected.")
+		end
+		event = host:service()
+	end
+	if not hosting then
+		print("sending")
+		send_stuff(server:index(), "hello", 5, 5.32)
+	end
 end
--- rpc.lua
+++ rpc.lua
@@ -7,197 +7,201 @@ local to_self_and_peer = "self_and_peer"
 local ffi = require("ffi")
 local encoders = {}
 encoders = {
-    ["byte"] = {
-        ["typecheck"] = function(object)
-            if type(object)=="string" and object:len()==1 then
-                return true
-            end
-            return false
-        end,
-        ["encode"] = function(byte)
-            return byte,1 -- characters used
-        end,
-        ["decode"] = function(str)
-            return str:sub(1,1),1 -- characters consumed
-        end
-    },
-    ["int"] = {
-        ["typecheck"] = function(object)
-            if type(object)=="number" and object==math.floor(object) then
-                return true
-            end
-            return false
-        end,
-        ["encode"] = function(int)
-            local encoded = ffi.string(ffi.new("int[?]",1, int), 4)
-            return encoded,4
-        end,
-        ["decode"] = function(str)
-            local ptr = ffi.cast("int*", ffi.new("char[?]", 4, str))
-            local t = ptr[0]
-            return t,4
-        end
-    },
-    ["float"] = {
-        ["typecheck"] = function(object)
-            if type(object)=="number" then
-                return true
-            end
-            return false
-        end,
-        ["encode"] = function(float)
-            local encoded = ffi.string(ffi.new("float[?]",1, float), 4)
-            return encoded,4
-        end,
-        ["decode"] = function(str)
-            local ptr = ffi.cast("float*", ffi.new("char[?]", 4, str))
-            local t = ptr[0]
-            return t,4
-        end
-    },
-    ["double"] = {
-        ["typecheck"] = function(object)
-            if type(object)=="number" then
-                return true
-            end
-            return false
-        end,
-        ["encode"] = function(double)
-            local encoded = ffi.string(ffi.new("double[?]",1, double), 8)
-            return encoded,8
-        end,
-        ["decode"] = function(str)
-            local ptr = ffi.cast("double*", ffi.new("char[?]", 8, str))
-            local t = ptr[0]
-            return t,8
-        end
-    },
-    ["string"] = {
-        ["typecheck"] = function(object)
-            if type(object) == "string" then
-                return true
-            end
-            return false
-        end,
-        ["encode"] = function(str)
-            local encoded_length = encoders.int.encode(str:len())
-            return encoded_length..str
-        end,
-        ["decode"] = function(str)
-            local length = str:sub(1,4)
-            length = encoders.int.decode(length)
-            return str:sub(5,4+length),4+length
-        end
-    }
+	["byte"] = {
+		["typecheck"] = function(object)
+			if type(object) == "string" and object:len() == 1 then
+				return true
+			end
+			return false
+		end,
+		["encode"] = function(byte)
+			return byte, 1 -- characters used
+		end,
+		["decode"] = function(str)
+			return str:sub(1, 1), 1 -- characters consumed
+		end,
+	},
+	["int"] = {
+		["typecheck"] = function(object)
+			if type(object) == "number" and object == math.floor(object) then
+				return true
+			end
+			return false
+		end,
+		["encode"] = function(int)
+			local encoded = ffi.string(ffi.new("int[?]", 1, int), 4)
+			return encoded, 4
+		end,
+		["decode"] = function(str)
+			local ptr = ffi.cast("int*", ffi.new("char[?]", 4, str))
+			local t = ptr[0]
+			return t, 4
+		end,
+	},
+	["float"] = {
+		["typecheck"] = function(object)
+			if type(object) == "number" then
+				return true
+			end
+			return false
+		end,
+		["encode"] = function(float)
+			local encoded = ffi.string(ffi.new("float[?]", 1, float), 4)
+			return encoded, 4
+		end,
+		["decode"] = function(str)
+			local ptr = ffi.cast("float*", ffi.new("char[?]", 4, str))
+			local t = ptr[0]
+			return t, 4
+		end,
+	},
+	["double"] = {
+		["typecheck"] = function(object)
+			if type(object) == "number" then
+				return true
+			end
+			return false
+		end,
+		["encode"] = function(double)
+			local encoded = ffi.string(ffi.new("double[?]", 1, double), 8)
+			return encoded, 8
+		end,
+		["decode"] = function(str)
+			local ptr = ffi.cast("double*", ffi.new("char[?]", 8, str))
+			local t = ptr[0]
+			return t, 8
+		end,
+	},
+	["string"] = {
+		["typecheck"] = function(object)
+			if type(object) == "string" then
+				return true
+			end
+			return false
+		end,
+		["encode"] = function(str)
+			local encoded_length = encoders.int.encode(str:len())
+			return encoded_length .. str
+		end,
+		["decode"] = function(str)
+			local length = str:sub(1, 4)
+			length = encoders.int.decode(length)
+			return str:sub(5, 4 + length), 4 + length
+		end,
+	},
 }
 
 local rpcs = {}
 
-function rpc_peer(name,host,peer_index,this_id,param_types,flag,...)
-    local args = {...}
-    local peer = host:get_peer(peer_index)
-    local msg = encoders["int"].encode(this_id)
-    for i, param in pairs(param_types) do
-        local encoder = encoders[param]
-        if encoder then
-            msg = msg .. encoder.encode(args[i])
-        else
-            error("no encoder for "..param)
-        end
-    end
-    print("msg:",msg)
-    print("peer_index:",peer_index)
-    print("peer:",peer)
-    peer:send(msg,nil,flag)
+function rpc_peer(host, peer_index, this_id, param_types, flag, ...)
+	local args = { ... }
+	local peer = host:get_peer(peer_index)
+	local msg = encoders["int"].encode(this_id)
+	for i, param in pairs(param_types) do
+		local encoder = encoders[param]
+		if encoder then
+			msg = msg .. encoder.encode(args[i])
+		else
+			error("no encoder for " .. param)
+		end
+	end
+	print("msg:", msg)
+	print("peer_index:", peer_index)
+	print("peer:", peer)
+	peer:send(msg, nil, flag)
 end
 
-function rpc_peers(host,this_id,param_types,flag,...)
-    local args = {...}
-    local msg = encoders["int"].encode(this_id)
-    for i, param in pairs(param_types) do
-        local encoder = encoders[param]
-        if encoder then
-            msg = msg .. encoder.encode(args[i])
-        else
-            error("no encoder for "..param)
-        end
-    end
-    host:broadcast(msg,nil,flag)
+function rpc_peers(host, this_id, param_types, flag, ...)
+	local args = { ... }
+	local msg = encoders["int"].encode(this_id)
+	for i, param in pairs(param_types) do
+		local encoder = encoders[param]
+		if encoder then
+			msg = msg .. encoder.encode(args[i])
+		else
+			error("no encoder for " .. param)
+		end
+	end
+	host:broadcast(msg, nil, flag)
 end
 local module = {}
 
 module.sender = nil
 
-function add_rpc(rpc_func,func,types)
-    table.insert(rpcs,{
-        ["rpc"]=rpc_func,
-        ["func"]=func,
-        ["types"]=types
-    })
+function add_rpc(rpc_func, func, types)
+	table.insert(rpcs, {
+		["rpc"] = rpc_func,
+		["func"] = func,
+		["types"] = types,
+	})
 end
 
 local ids = {}
 
-function module.new_rpc(receiver,param_types,flag,host,func)
-    if receiver == to_self_and_peer then
-        local this_id = #rpcs+1
-        local rpc_func = function(peer_index,...)
-            func(...)
-            peer_index = peer_index or 1
-            rpc_peer(host,peer_index,this_id,param_types,flag,...)
-        end
-        add_rpc(rpc_func,func,param_types)
-        return rpc_func
-    elseif receiver == to_self_and_peers then
-        local this_id = #rpcs+1
-        local rpc_func = function(...)
-            func(...)
-            rpc_peers(host,this_id,param_types,flag,...)
-        end
-        add_rpc(rpc_func,func,param_types)
-        return rpc_func
-    elseif receiver == to_peer then
-        print("to_peer")
-        local this_id = #rpcs+1
-        local rpc_func = function(peer_index,...)
-            peer_index = peer_index or 1
-            print("ble",peer_index,...)
-            rpc_peer(host,peer_index,this_id,param_types,flag,...)
-        end
-        add_rpc(rpc_func,func,param_types)
-        return rpc_func
-    elseif receiver == to_peers then
-        local this_id = #rpcs+1
-        local rpc_func = function(...)
-            rpc_peers(host,this_id,param_types,flag,...)
-        end
-        add_rpc(rpc_func,func,param_types)
-        return rpc_func
-    end
+function module.new_rpc(receiver, param_types, flag, host, func)
+	if receiver == to_self_and_peer then
+		local this_id = #rpcs + 1
+		local rpc_func = function(peer_index, ...)
+			func(...)
+			peer_index = peer_index or 1
+			rpc_peer(host, peer_index, this_id, param_types, flag, ...)
+		end
+		add_rpc(rpc_func, func, param_types)
+		return rpc_func
+	elseif receiver == to_self_and_peers then
+		local this_id = #rpcs + 1
+		local rpc_func = function(...)
+			func(...)
+			rpc_peers(host, this_id, param_types, flag, ...)
+		end
+		add_rpc(rpc_func, func, param_types)
+		return rpc_func
+	elseif receiver == to_peer then
+		print("to_peer")
+		local this_id = #rpcs + 1
+		local rpc_func = function(peer_index, ...)
+			peer_index = peer_index or 1
+			print("ble", peer_index, ...)
+			rpc_peer(host, peer_index, this_id, param_types, flag, ...)
+		end
+		add_rpc(rpc_func, func, param_types)
+		return rpc_func
+	elseif receiver == to_peers then
+		local this_id = #rpcs + 1
+		local rpc_func = function(...)
+			rpc_peers(host, this_id, param_types, flag, ...)
+		end
+		add_rpc(rpc_func, func, param_types)
+		return rpc_func
+	end
 end
 
-function module.listen_rpc(data,peer)
-    local args_data = data:sub(5,-1)
-    local id = encoders["int"].decode(data:sub(1,4))
-    local rpc = rpcs[id]
-    local args = {}
-    local buf = args_data
-    print("id:",id)
-    for _, arg_type in pairs(rpc.types) do
-        print(arg_type)
-        local encoder = encoders[arg_type]
-        print(encoder)
-        if not encoder then return end
-        local success, decoded, consumed = pcall(encoder.decode,buf)
-        if not success then return end
-        table.insert(args,decoded)
-        buf = buf:sub(1+consumed,-1)
-    end
-    module.sender = peer
-    local success,smth = pcall(rpc.func,unpack(args))
-    module.sender = nil
-    if not success then
-        print("caught error msg from rpc:",smth)
-    end
+function module.listen_rpc(data, peer)
+	local args_data = data:sub(5, -1)
+	local id = encoders["int"].decode(data:sub(1, 4))
+	local rpc = rpcs[id]
+	local args = {}
+	local buf = args_data
+	print("id:", id)
+	for _, arg_type in pairs(rpc.types) do
+		print(arg_type)
+		local encoder = encoders[arg_type]
+		print(encoder)
+		if not encoder then
+			return
+		end
+		local success, decoded, consumed = pcall(encoder.decode, buf)
+		if not success then
+			return
+		end
+		table.insert(args, decoded)
+		buf = buf:sub(1 + consumed, -1)
+	end
+	module.sender = peer
+	local success, smth = pcall(rpc.func, unpack(args))
+	module.sender = nil
+	if not success then
+		print("caught error msg from rpc:", smth)
+	end
 end
 return module
